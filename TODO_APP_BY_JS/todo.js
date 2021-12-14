8c2abf3b4f261db226532ffa3d2e0503dbd63dfe
Bhaskar Rai <bhaskar9893@gmail.com>
2021-12-14 19:38:44 +0530

    todo app in progress

--- TODO_APP_BY_JS/todo.js
+++ TODO_APP_BY_JS/todo.js
@@ -4,4 +4,27 @@ var input = document.querySelector(".todo_input");
 
 var main_todo_container = document.getElementById("todos");
 
-var addingButton = document.querySelector("i");
+var addingButton = document.querySelector(".add-item");
+
+var deleteAllButton = document.querySelector(".deleteBtn");
+
+addingButton.addEventListener("click", function(e){
+    //Creating all the elements--
+
+  if(input.value.trim()){
+      //ul tag
+      var ulTag = document.createElement("ul");
+      ulTag.classList.add(todo_list_container);
+  } 
+//todolist div (where we gonna append our new items or we can say our tasks)
+
+var todoList = document.createElement("div");
+todoList.classList.add("")
+  
+
+
+})
+
+deleteAllButton.addEventListener("click", function(){
+    console.log("buttonDEleteClicked");
+})
