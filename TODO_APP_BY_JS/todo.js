76a6369c84e1b22dc0cd2144120684bbca2e1179
Bhaskar Rai <bhaskar9893@gmail.com>
2021-12-14 20:50:34 +0530

    todo structure completed Ready to append on the DOM

--- TODO_APP_BY_JS/todo.js
+++ TODO_APP_BY_JS/todo.js
@@ -14,14 +14,52 @@ addingButton.addEventListener("click", function(e){
   if(input.value.trim()){
       //ul tag
       var ulTag = document.createElement("ul");
-      ulTag.classList.add(todo_list_container);
+      ulTag.classList.add("todo_list_container");
   } 
 //todolist div (where we gonna append our new items or we can say our tasks)
 
 var todoList = document.createElement("div");
-todoList.classList.add("")
-  
+todoList.classList.add("todo_list");
 
+//Li tag (where our input value gonna saved that is why we have to use innerHTML there);
+
+var liTag = document.createElement("li");
+liTag.innerHTML = input.value;
+liTag.classList.add("todo_item");
+
+//we need three buttons along with our tasks for editing and deleting and for mark completion.
+
+var buttonDiv = document.createElement("div");
+buttonDiv.classList.add("button");
+  
+//completion button.
+
+var completeButton = document.createElement("button");
+completeButton.classList.add("completed");
+completeButton.innerHTML ='<i class="fas fa-check-square"></i>'
+  
+//Edit Button
+
+var editButton = document.createElement("button");
+editButton.classList.add("editBtn");
+editButton.innerHTML ='<i class="fas fa-trash"></i>';
+
+//Trash BUtton
+
+var trashButton = document.createElement("button");
+trashButton.classList.add("trash");
+trashButton.innerHTML ='<i class="fas fa-trash"></i>';
+
+//Appending elements into each others----------------
+
+ulTag.appendChild(todoList);
+todoList.appendChild(liTag);
+todoList.appendChild(buttonDiv);
+buttonDiv.appendChild(completeButton);
+buttonDiv.appendChild(editButton);
+buttonDiv.appendChild(trashButton);
+
+console.log(ulTag);
 
 })
 
