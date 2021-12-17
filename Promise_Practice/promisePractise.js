dbe2cf6c20c0182c94ff4e93df852382c27363b8
Bhaskar Rai <bhaskar9893@gmail.com>
2021-12-17 21:41:44 +0530

    api fetching practice done

---
+++ Promise_Practice/promisePractise.js
@@ -0,0 +1,23 @@
+// Promise is basically a JS Object
+// const pobj1 = new Promise((resolve, reject) =>{
+//     setTimeout(() =>{
+// let rollNo = [1,2,3,4,5];
+// //resolve(rollNo);
+// reject("Error Occured Something Went Wrong")
+
+//     },2000)
+// })
+
+// pobj1.then((rollNo) =>{
+// console.log(rollNo); 
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+// fetch('https://api.covid19api.com/summary')
+// .then((apiData)=>{
+// console.log(apiData);
+//  }).catch((err)=>{
+//      console.log(err);
+//  })
+
